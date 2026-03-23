cd9a2b7b0fcfa0faf38d6da67dabc7a50a479e62
Pengcheng Zhang <65210538+pengchengz30@users.noreply.github.com>
2026-03-23 03:54:44 -0400

    Update versionCode to 32379 in build.gradle.kts

--- manager/app/build.gradle.kts
+++ manager/app/build.gradle.kts
@@ -110,7 +110,7 @@ android {
     defaultConfig {
         minSdk = androidMinSdkVersion
         targetSdk = androidTargetSdkVersion
-        versionCode = managerVersionCode
+        versionCode = 32379
         versionName = managerVersionName
 
         val isPrBuild = project.findProperty("IS_PR_BUILD")?.toString()?.toBoolean() ?: false
